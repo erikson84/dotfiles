3590bdf2736a5c23cad4ed702ee377b76addd8ed
Erikson Kaszubowski <erikson.kaszubowski@serpro.gov.br>
2025-09-05 09:29:17 -0300

    Re-add some updates to nvim and yasb

--- AppData/Local/nvim/lua/plugins/colorscheme.lua
+++ AppData/Local/nvim/lua/plugins/colorscheme.lua
@@ -1,61 +1,13 @@
 return {
   {
     "catppuccin/nvim",
-    lazy = true,
-    name = "catppuccin",
-    opts = {
-      transparent_background = false,
-      integrations = {
-        aerial = true,
-        alpha = true,
-        cmp = true,
-        blink_cmp = true,
-        dashboard = true,
-        dap = true,
-        dap_ui = true,
-        dropbar = {
-          enabled = true,
-          color_mode = false, -- enable color for kind's texts, not just kind's icons
-        },
-        flash = true,
-        gitsigns = true,
-        grug_far = true,
-        headlines = true,
-        illuminate = true,
-        indent_blankline = { enabled = true },
-        leap = true,
-        lsp_trouble = true,
-        mason = true,
-        markdown = true,
-        mini = true,
-        native_lsp = {
-          enabled = true,
-          underlines = {
-            errors = { "undercurl" },
-            hints = { "undercurl" },
-            warnings = { "undercurl" },
-            information = { "undercurl" },
-          },
-        },
-        navic = { enabled = true, custom_bg = "lualine" },
-        neotest = true,
-        neotree = true,
-        noice = true,
-        notify = true,
-        semantic_tokens = true,
-        snacks = true,
-        telescope = { enabled = true },
-        treesitter = true,
-        treesitter_context = true,
-        which_key = true,
-        barbecue = {
-          dim_dirname = true,
-          bold_basename = true,
-          dim_context = true,
-          alt_background = true,
-        },
-      },
-    },
+    opts = function(_, opts)
+      local module = require("catppuccin.groups.integrations.bufferline")
+      if module then
+        module.get = module.get_theme
+      end
+      return opts
+    end,
   },
   {
     "rebelot/kanagawa.nvim",
