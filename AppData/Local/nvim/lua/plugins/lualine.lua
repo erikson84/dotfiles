6b8511bf512214ffd8288bd1b6c57146b33a68bf
Erikson Kaszubowski <erikson.kaszubowski@serpro.gov.br>
2025-09-19 09:34:28 -0300

    Remove stale files from chezmoi

--- AppData/Local/nvim/lua/plugins/lualine.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "nvim-lualine/lualine.nvim",
-  opts = function(_, opts)
-    table.remove(opts.sections.lualine_c)
-    opts.options.theme = "catppuccin-mocha"
-  end,
-}
--- AppData/Local/nvim/lua/plugins/treesitter.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    highlight = {
-      enabled = true,
-      additional_vim_regex_highlighting = false,
-    },
-  },
-}
